47fe43a3c5249f311d311a38ebf7bdbb7e24ce7c
aswistak@linkpoint.ca <swistek.labs@gmail.com>
2020-10-25 23:26:28 -0700

    Added contract for multiple proof storage.

--- contracts/ProofOfExistence1.sol
+++ contracts/ProofOfExistence1.sol
@@ -2,6 +2,18 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract ProofOfExistence1 {
-  constructor() public {
+  // state
+  bytes32 public proof;
+
+  // calculate and store the poof for a document
+  // *transactional function*
+  function notarize(string memory document) public {
+    proof = proofFor(document);
+  }
+
+  // helper function to get a document's sha256
+  // *read-only function*
+  function proofFor(string memory document) public pure returns (bytes32) {
+    return sha256(abi.encodePacked(document));
   }
 }
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+let ProofOfExistence1 = artifacts.require('./ProofOfExistence1.sol');
+
+module.exports = function(deployer) {
+  deployer.deploy(ProofOfExistence1);
+};
